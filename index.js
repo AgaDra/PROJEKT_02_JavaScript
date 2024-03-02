0b1dffd11cd996ba7b88da64209016f1c7a994ae
AgaDra <agatymail@gmail.com>
2024-03-02 12:10:03 +0100

    readyVer

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const inputAmount = document.querySelector("#inputAmount");
-const btnCounter = document.querySelector("#btnCounter");
 const selector = document.querySelector("#selector");
 const result = document.querySelector("#result");
-const errorLabel = document.querySelector("#error-label");
+const form = document.querySelector("#currency-converter");
 
-const urlNbp =
-  "https://api.nbp.pl/api/exchangerates/tables/A/today/?format=json";
+const urlNbp = "https://api.nbp.pl/api/exchangerates/tables/A";
 
-const showError = () => {
-  errorLabel.textContent = "błąd pobierania danych";
-  document.querySelector("div").appendChild(errorLabel);
-};
-
-removeError = () => {
-  const error = document.querySelector("#error-label");
-  if (error) {
-    error.remove();
-  }
-};
-
-btnCounter.addEventListener("click", () => {
-  removeError();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const amount = event.target.inputAmount.value;
   const selectedCurrency = selector.value;
 
   fetch(urlNbp)
     .then((response) => {
+      if (!response.ok) {
+        throw new Error("Nie udało się pobrać kursu waluty");
+      }
       return response.json();
     })
     .then((data) => {
@@ -32,19 +22,11 @@ btnCounter.addEventListener("click", () => {
       data[0].rates.forEach((singleRate) => {
         rates[singleRate.code] = singleRate.mid;
       });
-
-      if (selectedCurrency in rates) {
-        const exchangeRate = rates[selectedCurrency];
-        const plnAmount = inputAmount.value;
-        if (plnAmount > 0) {
-          const resultExchange = plnAmount * exchangeRate;
-          result.textContent = `${resultExchange.toFixed(2)} PLN`;
-        } else {
-          result.textContent = "Wprowadź wartość większą od zera.";
-        }
-      } else {
-        result.textContent = "Wybrana waluta nie jest obsługiwana";
-      }
+      const exchangeRate = rates[selectedCurrency];
+      const resultExchange = amount * exchangeRate;
+      result.textContent = `${resultExchange.toFixed(2)} PLN`;
     })
-    .catch(() => showError());
+    .catch((error) => {
+      console.error("Błąd pobierania danych z NBP API:", error);
+    });
 });
