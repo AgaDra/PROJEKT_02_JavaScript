ba52647fe90bf9bd99b55a0c26d862a41efe72e7
AgaDra <agatymail@gmail.com>
2024-03-02 19:59:21 +0100

    lastversion

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const inputAmount = document.querySelector("#inputAmount");
-const selector = document.querySelector("#selector");
 const result = document.querySelector("#result");
 const form = document.querySelector("#currency-converter");
+const errorLabel = document.querySelector("#error-label");
 
 const urlNbp = "https://api.nbp.pl/api/exchangerates/tables/A";
 
+const showError = () => {
+  errorLabel.textContent = "Nie udało się pobrać kursu waluty!";
+};
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const amount = event.target.inputAmount.value;
-  const selectedCurrency = selector.value;
+  errorLabel.textContent = "";
+
+  const amount = event.target.amount.value;
+  const selectedCurrency = event.target.currency.value;
 
   fetch(urlNbp)
     .then((response) => {
       if (!response.ok) {
-        throw new Error("Nie udało się pobrać kursu waluty");
+        showError();
       }
       return response.json();
     })
     .then((data) => {
-      const rates = {};
-      data[0].rates.forEach((singleRate) => {
-        rates[singleRate.code] = singleRate.mid;
-      });
-      const exchangeRate = rates[selectedCurrency];
-      const resultExchange = amount * exchangeRate;
-      result.textContent = `${resultExchange.toFixed(2)} PLN`;
+      if (data.length > 0 && data[0].rates) {
+        const rates = {};
+        data[0].rates.forEach((singleRate) => {
+          rates[singleRate.code] = singleRate.mid;
+        });
+        const exchangeRate = rates[selectedCurrency];
+        const resultExchange = amount * exchangeRate;
+        result.textContent = `${resultExchange.toFixed(2)} PLN`;
+      } else {
+        showError();
+      }
     })
-    .catch((error) => {
-      console.error("Błąd pobierania danych z NBP API:", error);
+    .catch(() => {
+      showError();
     });
 });
